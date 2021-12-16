b67e6739402735b7281111cadcdeab055dd85958
waripoornima <waripoornimausa@gmail.com>
2021-12-16 13:56:49 -0800

    Update SpirentTemeva.py
    updated values

--- py_temeva_rest_client/SpirentTemeva.py
+++ py_temeva_rest_client/SpirentTemeva.py
@@ -352,7 +352,7 @@ class SpirentTemeva:
 def main():
     # user credentials
     user = 'poornima.wari@spirent.com'
-    password = 'Spirent2021'
+    password = 'Spirent123'
 
     # license server version end point
     end_points = '/lic/version'
